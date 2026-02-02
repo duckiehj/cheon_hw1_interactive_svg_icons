4ddd303ec539bed35e563cbf3eeb9b9132226877
Jin <duckiehj@gmail.com>
2026-02-02 00:32:17 -0500

    Add CSS
    add CSS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("JS file connected");
+
+const icons = document.querySelectorAll("#icon-container > div");
+
+icons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    console.log(`You clicked ${icon.id}`);
+  });
+});
